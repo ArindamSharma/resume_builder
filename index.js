584d8c45d98e3591bf2a7436d4d8d778c035474f
ArindamSharma <arindamsharma1998@gmail.com>
2022-09-05 21:30:31 +0530

    Added modal for filling in the resume details

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log(Skills)
+// console.log(Experiences)
+// console.log(Projects)
+// console.log(Achievements)
+function EmptyFormOpen(){
+    document.getElementsByClassName("modal")[0].classList.add("modal-show")
+}
+function ModalClose(){
+    document.getElementsByClassName("modal")[0].classList.remove("modal-show")
+}
+function ModalSave(){
+    
+}
