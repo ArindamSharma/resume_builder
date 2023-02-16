984fe6914ee2a9166ad37b6af9b558f4f38fe81a
ArindamSharma <36325272+ArindamSharma@users.noreply.github.com>
2023-02-16 13:39:22 +0000

    added template1 base structure

--- assets/js/template.js
+++ assets/js/template.js
@@ -19,6 +19,7 @@ function generateTemplate(element){
 
     container.appendChild(templatebody);
     container.appendChild(close);
+    // container.appendChild(print);
     return container;
 }
 function closeTemplate(element){
@@ -26,6 +27,11 @@ function closeTemplate(element){
     raiseFrame("main");
 }
 function openTemplate(id){
-    raiseFrame(id);
-    document.getElementById(id).appendChild(generateTemplate(templates[id]()));
+    if(Object.keys(userData)==0){
+        alert("No Data Found \nPlease redo step 2");
+    }
+    else{
+        raiseFrame(id);
+        document.getElementById(id).appendChild(generateTemplate(templates[id]()));
+    }
 }
--- assets/js/template1.js
+++ assets/js/template1.js
@@ -1,14 +1,52 @@
 templates['template1']=Template1;
 
+function temp1head(){
+    let container=document.createElement("div");
+    container.classList.add("temp1-head");
+
+    let name=document.createElement("label");
+    name.classList.add("temp1-head-name");
+    name.textContent=userData[INTRO][0][0]["value"]+" "+userData[INTRO][0][1]["value"];
+    container.appendChild(name);
+    
+    let title=document.createElement("label");
+    title.classList.add("temp1-head-title");
+    title.textContent=userData[INTRO][0][2]["value"];
+    container.appendChild(title);
+
+    return container;
+}
+function generateTemp1Section(data){
+    
+}
+function generateTemp1Category(categoryname,data){
+
+}
+function temp1left(){
+    let container=document.createElement("div");
+    container.classList.add("temp1-leftsection");
+    return container;
+}
+function temp1right(){
+    let container=document.createElement("div");
+    container.classList.add("temp1-rightsection");
+    return container;
+}
+function temp1body(){
+    let container=document.createElement("div");
+    container.classList.add("temp1-body");
+    container.appendChild(temp1left());
+    container.appendChild(temp1right());
+    return container;
+}
 function Template1(){
     
     let container=document.createElement("div");
     container.classList.add("temp1-container");
 
-    // Generate Page1
-    let page1=document.createElement("div");
-    page1.classList.add("page1");
+    container.appendChild(temp1head());
+    container.appendChild(temp1body());
 
-    container.appendChild(page1);
+    // container.appendChild(temp1);
     return container;
 }
