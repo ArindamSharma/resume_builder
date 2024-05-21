fa79c3a2f2ac556bad14c83c6954ee64484f9de2
ArindamSharma <36325272+ArindamSharma@users.noreply.github.com>
2024-05-21 10:14:03 +0000

    Improved the Main Page by updating button style and working on Form Page and overall color theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,10 @@ function raiseFrame(id){
     let slides = document.getElementsByClassName("frame");
     for (let i = 0; i < slides.length; i++) {
         slides.item(i).style.zIndex=1;
+        slides.item(i).style.display="none";
     }
     document.getElementById(id).style.zIndex=2;
+    document.getElementById(id).style.display="flex";
 }
 
 function OnLoad(){
